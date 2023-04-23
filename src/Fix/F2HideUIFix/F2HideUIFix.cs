c061caffc0a9665b720e0f1b396ade3a935dce8c
Jan Bureš <jan.bures@live.com>
2023-04-24 01:10:07 +0200

    Reworked F2 Hide UI Fix for game version 0.1.2

--- src/Fix/F2HideUIFix/F2HideUIFix.cs
+++ src/Fix/F2HideUIFix/F2HideUIFix.cs
@@ -1,31 +1,64 @@
 using HarmonyLib;
 using KSP.Game;
 using KSP.Messages;
-using KSP.UI.Flight;
+using KSP.Sim.impl;
+using KSP.UI;
+using UnityEngine;
 
 namespace CommunityFixes.Fix.F2HideUIFix;
 
 [Fix("F2 Hide UI Fix")]
-public class F2HideUIFix: BaseFix
+public class F2HideUIFix : BaseFix
 {
     public override void OnInitialized()
     {
         HarmonyInstance.PatchAll(typeof(F2HideUIFix));
+
+        _hideMapView = UIStateViews.HideFlightView.DeepCopy();
+
         Game.Messages.Subscribe<GameStateEnteredMessage>(msg =>
         {
             var message = (GameStateEnteredMessage)msg;
-            if (message.StateBeingEntered != GameState.FlightView && message.StateBeingEntered != GameState.Map3DView)
+            if (message.StateBeingEntered is GameState.FlightView or GameState.Map3DView)
             {
-                Game.UI.GetRootCanvas().worldCamera.enabled = true;
+                GameManager.Instance.Game.UI.GetRootCanvas()?.gameObject.transform.Find(
+                    "FlightHudRoot(Clone)/group_flightstaging(Clone)/StagingUI/Staging Stack"
+                )?.gameObject.SetActive(true);
             }
         });
     }
 
-    [HarmonyPatch(typeof(UIFlightHud), nameof(UIFlightHud.OnFlightHudCanvasActiveChanged))]
+    private static UIView _hideMapView;
+
+    [HarmonyPatch(typeof(UIStateViews), nameof(UIStateViews.HideFlightView), MethodType.Getter)]
     [HarmonyPostfix]
-    public static void UIFlightHud_OnFlightHudCanvasActiveChanged(bool isVisible)
+    // ReSharper disable once InconsistentNaming
+    public static void UIStateViews_HideFlightView_get(ref UIView __result)
     {
-        // Just disable the UI camera when F2 is pressed
-        GameManager.Instance.Game.UI.GetRootCanvas().worldCamera.enabled = isVisible;
+        __result.ViewDefinitions.Add(new UIViewElementDefinition
+        {
+            ID = "StagingStackOAB",
+            State = ViewState.Hidden
+        });
+    }
+
+    [HarmonyPatch(typeof(FlightInputHandler), nameof(FlightInputHandler.OnToggleFlightHUD))]
+    [HarmonyPostfix]
+    public static void FlightInputHandler_OnToggleFlightHUD()
+    {
+        var viewController = GameManager.Instance.Game.UI.ViewController;
+        if (viewController.CurrentView == UIStateViews.FlightMapView)
+        {
+            viewController.PushView(_hideMapView);
+        }
+        else if (viewController.CurrentView == _hideMapView)
+        {
+            viewController.PushView(UIStateViews.FlightMapView);
+        }
+
+        var stagingStack = GameManager.Instance.Game.UI.GetRootCanvas().gameObject.transform.Find(
+            "FlightHudRoot(Clone)/group_flightstaging(Clone)/StagingUI/Staging Stack"
+        ).gameObject;
+        stagingStack.SetActive(!stagingStack.activeSelf);
     }
 }
