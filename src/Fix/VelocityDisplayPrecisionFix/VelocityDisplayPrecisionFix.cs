f7d12176fa5de8095647c69c42970aea5e17467b
arthomnix <arthomnix@arthomnix.dev>
2023-06-23 21:42:22 +0100

    Add Velocity Display Precision Fix F2 Hide UI Fix has also been removed as it is now fixed in stock, and the fix does not build against the latest version of the game

---
+++ src/Fix/VelocityDisplayPrecisionFix/VelocityDisplayPrecisionFix.cs
@@ -0,0 +1,30 @@
+using KSP.Game;
+using KSP.UI.Binding;
+using UnityEngine;
+
+namespace CommunityFixes.Fix.VelocityDisplayPrecisionFix;
+
+[Fix("Velocity Display Precision Fix")]
+public class VelocityDisplayPrecisionFix: BaseFix
+{
+    private bool _fixed = false;
+    
+    public void Update()
+    {
+        var gameState = Game?.GlobalGameState?.GetGameState();
+        if (gameState == null || gameState.GameState is GameState.MainMenu or GameState.WarmUpLoading or GameState.Loading)
+            _fixed = false;
+        
+        if (_fixed) return;
+        
+        var velocityValue = GameObject.Find("GameManager/Default Game Instance(Clone)/UI Manager(Clone)/" +
+                                            "Scaled Main Canvas/FlightHudRoot(Clone)/group_navball(Clone)/Container/" +
+                                            "GRP-VEL/Container/DataContainer/Items/Value");
+        if (velocityValue == null) return;
+        var entries = velocityValue.GetComponent<UIValue_ReadNumber_TextUnits>().unitEntryArray;
+        for (int i = 0; i < entries.Length; ++i)
+            entries[i].dontTruncateValue = true;
+
+        _fixed = true;
+    }
+}
