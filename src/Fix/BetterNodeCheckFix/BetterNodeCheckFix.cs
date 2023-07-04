7ef3c4c094f03332c9720aaeb07c482bae5f397c
Steve Ratts <50781429+schlosrat@users.noreply.github.com>
2023-07-03 21:19:51 -0400

    Updated comments

--- src/Fix/BetterNodeCheckFix/BetterNodeCheckFix.cs
+++ src/Fix/BetterNodeCheckFix/BetterNodeCheckFix.cs
@@ -8,7 +8,7 @@ public class BetterNodeCheckFix : BaseFix
 {
   public static BetterNodeCheckFix Instance;
 
-  // Config parameters
+  // Community Fix config parameters specific to this fix
   internal static ConfigEntry<bool> _allowAllNodes;
 
   public BetterNodeCheckFix()
@@ -17,6 +17,7 @@ public class BetterNodeCheckFix : BaseFix
   }
   public override void OnInitialized()
   {
+    // Community Fix config parameters specific to this fix
     _allowAllNodes = CommunityFixesMod.Config.File.Bind<bool>("Better Node Check Fix", "Allow All", false, "If enabled, all new maneuver nodes are allowed. If disabled new and rebuilt nodes are limited by the combined engine and RCS DeltaV");
 
     HarmonyInstance.PatchAll(typeof(ManeuverPlanComponent_patches));
