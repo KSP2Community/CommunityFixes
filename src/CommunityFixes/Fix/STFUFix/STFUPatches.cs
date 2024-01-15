173184c81473d66b1c7ecd465268ab995c134d49
Jan Bureš <jan.bures@live.com>
2024-01-15 17:51:54 +0100

    Simplify STFUPatches and fix an apparent error

--- src/CommunityFixes/Fix/STFUFix/STFUPatches.cs
+++ src/CommunityFixes/Fix/STFUFix/STFUPatches.cs
@@ -1,8 +1,7 @@
 ﻿using HarmonyLib;
-using KSP.Game;
-using KSP.Logging;
 using KSP.Map;
 using KSP.Sim.impl;
+using SpaceWarp.API.Game;
 
 namespace CommunityFixes.Fix.STFUFix;
 
@@ -10,30 +9,9 @@ internal class STFUPatches
 {
     [HarmonyPatch(typeof(Map3DTrajectoryEvents), nameof(Map3DTrajectoryEvents.UpdateViewForOrbiter))]
     [HarmonyPrefix]
+    // ReSharper disable once InconsistentNaming
     public static bool BetterUpdateViewForOrbiter(Map3DTrajectoryEvents __instance, OrbiterComponent orbiter)
     {
-        if (orbiter == null)
-            GlobalLog.Warn("GenerateEventsForVessel() called with vessel.Orbiter == null! Events will not be updated");
-        else if (orbiter.PatchedConicSolver == null)
-        {
-            var activeVessel = GameManager.Instance?.Game?.ViewController?.GetActiveVehicle(true)?.GetSimVessel(true);
-            var currentTarget = activeVessel?.TargetObject;
-            if (!currentTarget.IsCelestialBody)
-                GlobalLog.Warn(
-                    "GenerateEventsForVessel() called with vessel.Orbiter.patchedConicSolver == null. Events will not be updated");
-        }
-        else if (__instance._mapCamera?.UnityCamera == null)
-        {
-            GlobalLog.Warn("GenerateEventsForVessel() called with a null map camera. Events will not be updated");
-        }
-        else
-        {
-            IGGuid globalId = orbiter.SimulationObject.GlobalId;
-            __instance.UpdateViewForCurrentTrajectory(orbiter, globalId);
-            __instance.UpdateViewForManeuverTrajectory(orbiter, globalId);
-            __instance.UpdateViewForTargeter(orbiter.OrbitTargeter, orbiter, globalId);
-        }
-
-        return false;
+        return orbiter.PatchedConicSolver != null || Vehicle.ActiveSimVessel?.TargetObject?.IsCelestialBody is not true;
     }
 }
